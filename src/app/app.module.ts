6607b06dea2b2e6fb284079cb8276a40a838e3fc
Thiago Attico da Silva <thiago.attico@itlab.com.br>
2022-06-09 15:31:58 -0300

    Criação da dashboard principal e componentes do header e do form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './shared/header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormTransactionComponent } from './form-transaction/form-transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    FormTransactionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
